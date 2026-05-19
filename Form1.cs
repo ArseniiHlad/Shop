cfd51e21b01cd53584776d4f1cddbc203125c81c
Arsenii Hladkyykh <arsenii.hladkykh@nure.ua>
2026-05-19 21:33:17 +0300

    Старт курсовой. Сделал чистое главное окно

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Kursach
+{
+    public class Form1 : Form
+    {
+        public Form1()
+        {
+            this.Text = "Магазин - Касовий Апарат";
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
